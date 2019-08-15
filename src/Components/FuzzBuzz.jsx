8ae5b65efd90449bbac1739712681368f4e02cce
Wendy <wgaul102@uottawa.ca>
2019-08-14 21:31:27 -0400

    Add FizzBuzz

--- src/Components/FuzzBuzz.jsx
+++ src/Components/FuzzBuzz.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
 export default class FuzzBuss extends React.Component {
+
+    createTable = () => {
+        let table = [];
+        for (var i = 1; i <= 100; i++) {
+            table.push(<tr>
+                <td>{i}</td>
+                <td>{this.createWort(i)}</td>
+            </tr>)
+        }
+        return table;
+    }
+
+    createWort = (i) => {
+        let answer = "";
+        if (i % 3 === 0) {
+            answer += "Fizz";
+        }
+        if (i % 5 === 0) {
+            answer += "Buzz";
+        }
+        return answer;
+    }
     render() {
+
+        for (var i = 1; i <= 100; i++) {
+
+        }
+
         return (
-            <div>Hello</div>
+            <>
+                <h1>This is the FizzBuzz</h1>
+                <div><p>Create the numbers 1 to 100. If the number is divided by 3, write Fizz, if it's divided by 5 write Buzz.</p></div>
+                <table>{this.createTable()}</table>
+            </>
         )
     }
 }

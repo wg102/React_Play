618f2cc8d49b52e1e0498753df6182559ac4d3ac
wg102 <wgaul102@uottawa.ca>
2019-08-18 11:48:35 -0400

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import FuzzBuzz from './Components/FuzzBuzz';
+import FlexBox from './Components/FlexBox';
+import Notfound from './Components/NotFound';
+import HomePage from "./Layout/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <FuzzBuzz />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/FuzzBuzz" component={FuzzBuzz} />
+        <Route component={Notfound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 

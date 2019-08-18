c87dfad7723a6473561b96416cb810ae09a30084
wg102 <wgaul102@uottawa.ca>
2019-08-18 17:45:28 -0400

    Create Service, modify to use class component

--- src/Components/SocialCard.jsx
+++ src/Components/SocialCard.jsx
@@ -1,43 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CardService from '../service/cardService';
 
 import '../style/socialCard.scss';
 
 
-const SocialCard = ({ title, picture, description }) => (
-    <article className="card">
-        <h1>{title}</h1>
-        <img
-            src={picture}
-            alt="preview"
-        />
-        <p>{description}</p>
-    </article>
-);
 
-function createSocialCards() {
-    let cards = [];
-    for (var i = 0; i < 4; i++) {
-        cards.push(<SocialCard />);
+export default class SocialCard extends React.Component {
+    constructor() {
+        super();
+        this.state = { cards: {} };
     }
-    return cards;
+
+    componentDidMount() {
+        //Attach the fetch method
+        this.setState({ cards: CardService.getCards() });
+    }
+
+    render() {
+        const { cards } = this.state;
+        const cardsToShow = [];
+
+        const SocialCardss = ({ title, picture, description }) => (
+            <article className="card">
+                <h1>{title}</h1>
+                <img
+                    src={picture}
+                    alt="preview"
+                />
+                <p>{description}</p>
+            </article>
+        );
+
+        SocialCardss.propTypes = {
+            title: PropTypes.string,
+            picture: PropTypes.string,
+            description: PropTypes.string
+        }
+
+        SocialCardss.defaultProps = {
+            title: "Hello",
+            picture: '/logo512.png',
+            description: 'whiteSectionText'
+        }
+
+        for (var i = 0; i < cards.length; i++) {
+            cardsToShow.push(<SocialCardss title={cards[i].title} picture={cards[i].picture} description={cards[i].description} />);
+        }
+        return (
+            <section className="cardHolder">
+                {cardsToShow}
+            </section>
+        );
+    }
 }
-
-const SocialCards = () => (
-    <section className="cardHolder">
-        {createSocialCards()}
-    </section>
-);
-
-SocialCard.propTypes = {
-    title: PropTypes.string,
-    picture: PropTypes.string,
-    description: PropTypes.string
-};
-
-SocialCard.defaultProps = {
-    title: "Hello",
-    picture: '/logo512.png',
-    description: 'whiteSectionText'
-};
-export default SocialCards

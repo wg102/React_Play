97fe6d7ccb97469289ff0f60dc5c8d7f2874d748
wg102 <wgaul102@uottawa.ca>
2019-08-18 11:50:40 -0400

    Add Router files

---
+++ src/Components/FlexBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../style/socialCard.scss';
+
+const FlexBox = () => {
+    return (
+        <>
+            <ul class="flex-container">
+                <li class="flex-item">1</li>
+                <li class="flex-item">2</li>
+                <li class="flex-item">3</li>
+                <li class="flex-item">4</li>
+                <li class="flex-item">5</li>
+                <li class="flex-item">6</li>
+            </ul>
+        </>
+    );
+}
+
+export default FlexBox;

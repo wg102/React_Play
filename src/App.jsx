640fea9123d710e8bd2e4cd765e8254fcabc8d9e
wg102 <wgaul102@uottawa.ca>
2019-08-20 20:58:02 -0400

    Created basic Calculator shape as a table

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import FuzzBuzz from './Components/FuzzBuzz';
 import SocialCard from './Components/SocialCard';
 import Notfound from './Components/NotFound';
 import Stack from './Components/Stack';
 import HomePage from './Layout/Home';
+import CalulatriceBasic from './Components/CalculatriceBasic';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/CalcBasic" component={CalulatriceBasic} />
         <Route path="/FuzzBuzz" component={FuzzBuzz} />
         <Route path="/SocialCard" component={SocialCard} />
         <Route path="/Stack" component={Stack} />
